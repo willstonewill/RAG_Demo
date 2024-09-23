702da1a3e5b4f5b38197bcf82d41f5fe0ae7de4e
willstonewill <lijianshi1993@gmail.com>
2024-09-23 19:21:15 -0400

    Update RAG.py

--- RAG.py
+++ RAG.py
@@ -79,7 +79,8 @@ def user_input(user_question, api_key, chat_history):
     retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 10})
 
     rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough(), MessagesPlaceholder("chat_history")}
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | MessagesPlaceholder("chat_history")
     | prompt
     | model
     | StrOutputParser()
