9b2df47df9f5416391163bf099b54240a53ac8aa
willstonewill <lijianshi1993@gmail.com>
2024-09-23 15:10:02 -0400

    Update RAG.py

--- RAG.py
+++ RAG.py
@@ -1,7 +1,3 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
 import streamlit as st
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_experimental.text_splitter import SemanticChunker
