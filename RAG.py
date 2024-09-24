85a03c6fabd65f65e1201c15eaeb5399464f3142
willstonewill <lijianshi1993@gmail.com>
2024-09-24 13:52:06 -0400

    Update RAG.py

--- RAG.py
+++ RAG.py
@@ -124,7 +124,7 @@ def user_input(user_question, api_key, chat_history):
     | StrOutputParser()
     )
     
-    st.write(get_chathistory(chat_history))
+    st.write(get_chathistory(list(chat_history)))
 
     response = ""
     for chunk in rag_chain.stream(user_question):
