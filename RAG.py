03617d6f8b6d38d7698baaeeca5b90667bf7acc6
willstonewill <lijianshi1993@gmail.com>
2024-09-24 10:04:40 -0400

    Update RAG.py

--- RAG.py
+++ RAG.py
@@ -76,7 +76,7 @@ def user_input(user_question, api_key, chat_history):
 
     Answer:
     """
-    model = ChatOpenAI(model="gpt-4o-mini", temperature=0, api_key=api_key)
+    model = ChatOpenAI(model="gpt-4o-mini", temperature=1, api_key=api_key)
     prompt = ChatPromptTemplate.from_template(system_prompt)
     embedding = OpenAIEmbeddings(openai_api_key=api_key)
     #vectorstore = Chroma(persist_directory="./chroma_db", embedding_function=embedding)
@@ -84,7 +84,7 @@ def user_input(user_question, api_key, chat_history):
     retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 10})
 
     rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough(), "chat_history": RunnablePassthrough()}
+    {"context": retriever | format_docs, "question": RunnablePassthrough(), "chat_history": st.session_state["chat_history"]}
     | prompt
     | model
     | StrOutputParser()
